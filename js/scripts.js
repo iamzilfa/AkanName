12ae46e67d99d97deeeb394d26bd732127b74e39
Zilfa Cyamani <zilcyam@gmail.com>
2020-09-27 23:25:47 +0200

    assigned gender to day in js

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,45 @@ function names() {
     var dateDig = parseInt(document.getElementById("date").value);
     var day = (((centuryDig / 4) - 2 * centuryDig - 1) + ((5 * yearDig / 4)) + ((26 * (monthDig + 1) / 10)) + dateDig) % 7;
     console.log((Math.floor(day)));
+
+    var genders = document.getElementById("gender").value
+    if (genders == "male") {
+        var akanName;
+        if (day == 1) {
+            akanName = "Kwadwo";
+        } else if (day == 2) {
+            akanName = "Kwabena";
+        } else if (day == 3) {
+            akanName = "Kwaku";
+        } else if (day == 4) {
+            akanName = "Yaw";
+        } else if (day == 5) {
+            akanName = "Kofi";
+        } else if (day == 6) {
+            akanName = "Kwame";
+        } else if (day == 7) {
+            akanName = "Kwasi"
+        }
+    }
+
+    else {
+        if (day == 1) {
+            akanName = "Adwoa";
+        } else if (day == 2) {
+            akanName = "Abenaa";
+        } else if (day == 3) {
+            akanName = "Akua";
+        } else if (day == 4) {
+            akanName = "Yaa";
+        } else if (day == 5) {
+            akanName = "Afua";
+        } else if (day == 6) {
+            akanName = "Ama";
+        } else if (day == 7) {
+            akanName = "Akosua";
+        }
+    }
+
+    document.getElementById("display").innerHTML = "Your Akan Name is " + akanName;
+
+}
