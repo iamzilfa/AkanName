741da2e5eb640bc691740de39e9444db79c733bf
Zilfa Cyamani <zilcyam@gmail.com>
2020-09-28 17:47:40 +0200

    Js edited

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,37 @@
+
 function names() {
-    year = document.getElementById("year").value;
+    var year = document.getElementById("year").value;
     var centuryDig = parseInt(year.substring(0, 2));
     var yearDig = parseInt(year.substring(2, 4));
     var monthDig = parseInt(document.getElementById("month").value);
     var dateDig = parseInt(document.getElementById("date").value);
     var day = (((centuryDig / 4) - 2 * centuryDig - 1) + ((5 * yearDig / 4)) + ((26 * (monthDig + 1) / 10)) + dateDig) % 7;
-    console.log((Math.floor(day)));
 
-    var genders = document.getElementById("gender").value
-    if (genders == "male") {
-        var akanName;
+    if (year == "") {
+        alert("Not valid");
+        return false;
+    }
+    if (monthDig == "") {
+        alert("Not invalid");
+        return false;
+    }
+    if (monthDig > 12 || monthDig <= 0) {
+        alert("Not valid");
+        return false;
+    }
+    if (dateDig == "") {
+        alert("Not valid");
+        return false;
+    }
+    if (dateDig > 31 || dateDig <= 0) {
+        alert("Not valid");
+        return false;
+    }
+
+    var myGender = document.getElementById("gender").value
+    var akanName;
+
+    if (myGender == "Male") {
         if (day == 1) {
             akanName = "Kwadwo";
         } else if (day == 2) {
@@ -22,7 +44,7 @@ function names() {
             akanName = "Kofi";
         } else if (day == 6) {
             akanName = "Kwame";
-        } else if (day == 7) {
+        } else {
             akanName = "Kwasi"
         }
     }
@@ -40,7 +62,7 @@ function names() {
             akanName = "Afua";
         } else if (day == 6) {
             akanName = "Ama";
-        } else if (day == 7) {
+        } else {
             akanName = "Akosua";
         }
     }
